1bfcdbf8e8e971bae44ee02073bfb68f727234f6
beomsu <qja1397@gmail.com>
2026-08-01 23:51:44 +0900

    beomsu

---
+++ src/pages/CompanyDnaPage.tsx
@@ -0,0 +1,9 @@
+function CompanyDnaPage() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Company DNA</h1>
+    </section>
+  )
+}
+
+export default CompanyDnaPage
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+function DashboardPage() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+    </section>
+  )
+}
+
+export default DashboardPage
